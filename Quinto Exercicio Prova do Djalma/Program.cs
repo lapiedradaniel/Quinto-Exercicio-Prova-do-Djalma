c8edad92195fa845c2b92f251c38afb7f9681251
daniel santos <danielsantosaraujo2010@hotmail.com>
2022-07-03 20:42:49 -0300

    Adicionar arquivos de projeto.

---
+++ Quinto Exercicio Prova do Djalma/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quinto_Exercicio_Prova_do_Djalma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+
+            while (i <= 100) 
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+
+
+            
+
+
+                i++;
+                
+            }
+            Console.ReadKey();
+
+
+        }
+
+
+    }
+}
